0859463f3a397fadb2993ae4656f361e7ced266c
hamza-25 <me677499@gmail.com>
2024-01-06 17:44:35 +0100

    add pass to a func

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -28,3 +28,9 @@ def delete_state(state_id):
     storage.delete(state)
     storage.save()
     return make_response(jsonify({}), 200)
+
+app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    pass
+    
+
