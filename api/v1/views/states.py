28b2830d887e8d153294bc6dddb016d76a5ce62e
hamza-25 <me677499@gmail.com>
2024-01-06 18:03:16 +0100

    complete task 6 code write

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from models import storage
 from models.state import State
-from flask import Flask, Blueprint, jsonify, abort, make_response
+from flask import Flask, Blueprint, jsonify, abort, make_response, request
 from api.v1.views import app_views
 
 app_views.route('/states' , methods=['GET'], strict_slashes=False)
@@ -31,6 +31,27 @@ def delete_state(state_id):
 
 app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
-    pass
-    
+    if not request.get_json():
+        abort(400, description='Not a JSON')
+    if 'name' not in request.get_json():
+        abort(400, description='Missing name')
+    data = request.get_json()
+    new_state = State(**data)
+    new_state.save()
+    return make_response(jsonify(new_state.to_dict()), 201)
 
+app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    state = storage.get(State, state)
+    if not state:
+        abort(404)
+    if not request.get_json():
+        abort(400, description='Not a JSON')
+    data = request.get_json()
+    ignore_keys = ['id', 'created_at ', 'updated_at']
+    for k, v in data.items():
+        if k not in ignore_keys:
+            setattr(state, k, v)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 200)
+   
