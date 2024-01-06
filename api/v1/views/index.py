67317ea055b06d196654635b8346a30a7c77e71d
amyn00 <medamine190@gmail.com>
2024-01-06 14:50:11 +0000

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
 """Index file to connect to API."""
+from models import storage
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
+@app_views.route('/stats')
+def get_stats():
+    """Retrive the number of each object by type."""
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
 
 @app_views.route('/status')
 def status():
