9c131738b0471e351ef509cc65ca6b4344d339ce
hamza-25 <me677499@gmail.com>
2024-01-07 23:27:50 +0100

    add prefix amenities.py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -6,11 +6,8 @@ from flask import Flask, Blueprint, jsonify, abort, make_response, request
 from api.v1.views import app_views
 
 
-# app = Flask(__name__)
-# app_views = Blueprint('app_views', __name__)
-
-
-@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+@app_views.route('/api/v1/amenities',
+                 methods=['GET'], strict_slashes=False)
 def get_amenities():
     """get all amenities object"""
     all_amenities = storage.all(Amenity).values()
@@ -20,8 +17,8 @@ def get_amenities():
     return jsonify(new_list)
 
 
-@app_views.route('/amenities/<amenity_id>',
-                methods=['GET'], strict_slashes=False)
+@app_views.route('/api/v1/amenities/<amenity_id>',
+                 methods=['GET'], strict_slashes=False)
 def get_amenity_id(amenity_id):
     """get amenity by id"""
     amenity = storage.get(Amenity, amenity_id)
@@ -30,8 +27,8 @@ def get_amenity_id(amenity_id):
     return jsonify(amenity.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>',
-                methods=['DELETE'], strict_slashes=False)
+@app_views.route('/api/v1/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_amenity(amenity_id):
     """delete state from engine storage"""
     amenity = storage.get(Amenity, amenity_id)
@@ -42,7 +39,7 @@ def delete_amenity(amenity_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+@app_views.route('/api/v1/amenities', methods=['POST'], strict_slashes=False)
 def create_amenity():
     """update amenities from engine storage"""
     if not request.get_json():
@@ -55,8 +52,8 @@ def create_amenity():
     return make_response(jsonify(new_amenity.to_dict()), 201)
 
 
-@app_views.route('/amenities/<amenity_id>',
-                methods=['PUT'], strict_slashes=False)
+@app_views.route('/api/v1/amenities/<amenity_id>',
+                 methods=['PUT'], strict_slashes=False)
 def update_amenity(amenity_id):
     """update amenity by id"""
     amenity = storage.get(Amenity, amenity_id)
