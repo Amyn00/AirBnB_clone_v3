535476980db3bfb4f61ce9d08698cd28abd67c60
hamza-25 <me677499@gmail.com>
2024-01-07 18:19:47 +0100

    add CORS  to app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,11 @@ import os
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
