95281b273d67d07a985e4160169b87d701a1c2e9
hamza-25 <me677499@gmail.com>
2024-01-07 21:58:42 +0100

    fix init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,7 @@
 """Init Blieprint views."""
 from flask import Blueprint, Flask
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/vi')
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.cities import *
