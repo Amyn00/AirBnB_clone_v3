3bc12fcf0ec1015753de72323d5f5aa1a1a02d59
amyn00 <medamine190@gmail.com>
2024-01-08 07:27:37 +0000

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,13 +2,14 @@
 """Connect to API"""
 import os
 from models import storage
-from api.v1.views.states import app_views
+# from api.v1.views.states import app_views
+from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
 
 
 app = Flask(__name__)
-#app.register_blueprint(app_views, url_prefix='/api/v1')
+# app.register_blueprint(app_views, url_prefix="/api/v1")
 app.register_blueprint(app_views)
 cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
@@ -28,4 +29,4 @@ def not_found(error):
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, threaded=True)
